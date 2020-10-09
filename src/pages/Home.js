32ebe5f01cc31c53dadf9d4630cf15e871549c18
Balbaal <iqbal.mi20@gmail.com>
2020-10-09 15:49:55 +0800

    add: hiding navigation when scroll

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,38 @@ import { NavButton, TimeLine } from "parts";
 import { getDaysInMonth } from "utils";
 
 class Home extends React.Component {
+  componentDidMount() {
+    // hide navigation when scroll down
+    const elSelect = document.querySelector(".nav-button");
+    let prevScroll = window.pageYOffset;
+
+    window.addEventListener("scroll", () => {
+      let currentScroll = window.pageYOffset;
+
+      if (prevScroll < currentScroll) {
+        elSelect.classList.add("hide");
+      } else if (prevScroll > currentScroll) {
+        elSelect.classList.remove("hide");
+      }
+
+      console.log("prevScroll :>> ", prevScroll);
+      console.log("currentScroll :>> ", currentScroll);
+      prevScroll = currentScroll;
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <div
           className="shadow shadow-sm"
-          style={{ margin: "0 -15px 32px", padding: "8px 15px" }}
+          style={{
+            margin: "0 -15px 32px",
+            padding: "8px 15px",
+            position: "sticky",
+            backgroundColor: "white",
+            top: 0,
+          }}
         >
           <CtaBack />
           <DatePicker
